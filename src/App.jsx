5c221ca32bcdbbae8afaaba0d855df1ade01099f
Sami HH <samisedhh@gmail.com>
2022-11-28 14:10:12 +0100

    first commit!

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,11 @@
-import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import './App.css'
-// import Map from './components/map/Map'
-import Home from './components/map/Map-2'
+import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
   
-
   return (
-    <div className="App">
-      
+    <div className="App">     
       <h1 > Hello my location </h1>
       <Map />
-
-        {/* <Home/>  */}
-
     </div>
   )
 }
